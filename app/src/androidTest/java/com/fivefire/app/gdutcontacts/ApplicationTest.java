a64c5d668cbf6382563fc7aad47ec92c76d03777
microstudent <455173472@qq.com>
2016-05-11 23:31:15 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/fivefire/app/gdutcontacts/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.fivefire.app.gdutcontacts;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
