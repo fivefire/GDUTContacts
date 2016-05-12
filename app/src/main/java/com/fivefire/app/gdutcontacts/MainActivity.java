41a39803ed574dc05ac2685ebb8ed0c14a82f51f
dengjiangdun <1414724967@qq.com>
2016-05-12 22:13:56 +0800

    add the bomb ,which keep the data to server

--- app/src/main/java/com/fivefire/app/gdutcontacts/MainActivity.java
+++ app/src/main/java/com/fivefire/app/gdutcontacts/MainActivity.java
@@ -3,11 +3,14 @@ package com.fivefire.app.gdutcontacts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.bmob.v3.Bmob;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Bmob.initialize(this,"58d2bb059cc1244e252cea21b4313d0c");
     }
 }
