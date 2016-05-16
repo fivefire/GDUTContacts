b097a61963a9dd1480b1fc7f3e76b0291e775929
LanyLin <605857467@qq.com>
2016-05-16 14:41:45 +0800

    完成个人信息页和搜索页

--- app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/ClassifyAcativity.java
+++ app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/ClassifyAcativity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,17 +13,25 @@ import android.widget.ListView;
 import com.fivefire.app.gdutcontacts.R;
 import com.fivefire.app.gdutcontacts.ui.common.BaseFragment;
 
+import cn.bmob.v3.Bmob;
+
 /**
  * 分类页面
  */
 public class ClassifyAcativity extends BaseFragment.BaseActivity {
-
+    private Toolbar mtoolbar;
     private String[] Contenx;
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void setupView(Bundle savedInstanceState) {
+
+        if (mtoolbar != null) {
+            setSupportActionBar(mtoolbar);
+            setTitle(getString(R.string.app_name));
+        }
+
         Intent intent=getIntent();
         String massage = intent.getStringExtra("Message");
         Resources resources =getResources();
@@ -46,7 +55,8 @@ public class ClassifyAcativity extends BaseFragment.BaseActivity {
 
     @Override
    protected void initView() {
-
+        mtoolbar = (Toolbar)findViewById(R.id.toolbar);
+        Bmob.initialize(this,"58d2bb059cc1244e252cea21b4313d0c");
         listView = (ListView)findViewById(R.id.listView);
 
    }
