85c56472e5e09f88dd7841d7eb1c052656bddd5d
754170571@qq.com <qq7541>
2016-05-18 23:23:18 +0800

    AndroidManifest文件更改了从LoginActivity开始，按下登录跳转到MainActivity，还修改了按钮样式问题

--- app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/LoginActivity.java
+++ app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.fivefire.app.gdutcontacts.ui.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.fivefire.app.gdutcontacts.R;
@@ -10,6 +15,7 @@ import com.fivefire.app.gdutcontacts.ui.common.ClearEditText;
 
 public class LoginActivity extends Activity {
     private ClearEditText loginUser,loginPassword;
+    private Button btn_signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,36 @@ public class LoginActivity extends Activity {
         loginUser.setCompoundDrawables(d1,null,null,null);
         loginPassword = (ClearEditText)findViewById(R.id.login_password);
         loginPassword.setCompoundDrawables(d2,null,null,null);
-    }
 
+        btn_signIn = (Button)findViewById(R.id.signIn);
+        btn_signIn.setOnClickListener(new LoginAction());
+    }
+    class LoginAction implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.signIn :
+                    Intent intent = new Intent();
+                    intent.setClass(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.login_forgot_pass:
+
+                    break;
+                case R.id.login_register:
+
+                    break;
+                case R.id.login_username:
+
+                    break;
+                case R.id.login_password:
+
+                    break;
+                default:
+                    Toast.makeText(getApplication(),"Error",Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
 }
