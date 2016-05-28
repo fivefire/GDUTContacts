f62518341c755731f68ea56fff0dc6eb246155c2
guangmomo <1853959636@qq.com>
2016-05-28 20:31:08 +0800

    修复了Verify的BUG

--- app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/VerifyActivity.java
+++ app/src/main/java/com/fivefire/app/gdutcontacts/ui/activity/VerifyActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.fivefire.app.gdutcontacts.R;
@@ -84,6 +85,15 @@ public class VerifyActivity extends BaseActivity implements SwipeRefreshLayout.O
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setTitle(R.string.verify);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
